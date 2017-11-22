3b6015291be5af27516a5ff0ebe4e9ab79e722a6
Siqueira <SiqueiT@moodys.com>
2017-11-22 18:40:38 -0500

    initial source code for jenkins

---
+++ HelloJenkins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloJenkins
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello Jenkins!";
+        }
+    }
+}
---
+++ HelloJenkinsTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloJenkins;
+
+namespace HelloJenkinsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SimonSaysHelloJenkins()
+        {
+            Assert.AreEqual("Hello Jenkins!", Program.CreateMessage());
+        }
+    }
+}
